2dcc05d6fdc829554ab57ad55d54ca9fb01608ca
Joris Deguet <joris.deguet@gmail.com>
2016-06-11 16:51:42 -0400

    Cleanup et ajout d'un exemple sur la serialisation avec Gson
    Supprimer des vieux tests copiés collés dans les autres projets.
    Ajouter un exemple de Gson custom.

--- code/https/HttpsServer/src/test/java/org/deguet/tests/client/BaseURL.java
+++
@@ -1,8 +0,0 @@
-package org.deguet.tests.client;
-
-public class BaseURL {
-
-	public static final String base = "https://localhost:7030/rest/";
-	public static final String di5a5 = "https://5a5.di.college-em.info:7030/rest/";
-	
-}
