6f89bbb794683ed4827db0f1d4d650352b1bb7e5
Joris Deguet <joris.deguet@gmail.com>
2017-10-06 12:08:37 -0400

    Improve Progress and fix the lost build.gradle

---
+++ code/Progress/app/src/main/java/org/deguet/progress/MainActivity.java
@@ -0,0 +1,89 @@
+package org.deguet.progress;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ProgressBar progress;
+    TextView stuff;
+
+    ProgressDialog progressD;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        progress = (ProgressBar) findViewById(R.id.progress);
+        stuff = (TextView) findViewById(R.id.stuff);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDownload();
+                new TTask<>().execute();
+            }
+        });
+
+        // show progress
+        progressD = ProgressDialog.show(MainActivity.this, "Please wait",
+                "Long operation starts...", true);
+        // start the task that will stop it
+        new DialogTask<>().execute();
+    }
+
+    private void startDownload(){
+        progress.setVisibility(View.VISIBLE);
+        stuff.setVisibility(View.INVISIBLE);
+    }
+
+    private void endDownload(){
+        progress.setVisibility(View.INVISIBLE);
+        stuff.setVisibility(View.VISIBLE);
+    }
+
+    class TTask<A,B,C> extends AsyncTask<A,B,C> {
+
+        @Override
+        protected void onPostExecute(C c) {
+            endDownload();
+            super.onPostExecute(c);
+        }
+
+        @Override
+        protected C doInBackground(A... params) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+    class DialogTask<A,B,C> extends AsyncTask<A,B,C> {
+
+        @Override
+        protected void onPostExecute(C c) {
+            progressD.hide();
+            super.onPostExecute(c);
+        }
+
+        @Override
+        protected C doInBackground(A... params) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+}
